395126262351c239df8ae27bae2f27502a8c76a8
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-11 19:25:28 +0530

    products model created

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'description',
+                    'create_date', 'write_date')
+    list_display_links = ('id', 'name', 'price', 'description',
+                          'create_date', 'write_date')
--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.FloatField(default=0.0)
+    description = models.CharField(max_length=250, default='')
+    image = models.ImageField(upload_to='uploads/products/')
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
