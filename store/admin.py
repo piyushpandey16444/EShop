e4d89713e66ec371bfb50fd4289d74e17398cf61
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-11 19:31:13 +0530

    admin updated

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'description',
+    list_display = ('id', 'name', 'category', 'price', 'description',
                     'create_date', 'write_date')
-    list_display_links = ('id', 'name', 'price', 'description',
+    list_display_links = ('id', 'name', 'category', 'price', 'description',
                           'create_date', 'write_date')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'create_date', 'write_date')
+    list_display_links = ('id', 'name', 'create_date', 'write_date')
--- store/models.py
+++ store/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    category = models.ForeignKey(
+        "store.Category", on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=250)
     price = models.FloatField(default=0.0)
     description = models.CharField(max_length=250, default='')
