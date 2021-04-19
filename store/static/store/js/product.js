8001626be3507a3fcba235111c1b6ed8b1e2664a
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-19 18:03:21 +0530

    ajax implimented

--- store/static/store/js/product.js
+++ store/static/store/js/product.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
     $(".list__filter").click(function(e) {
         e.preventDefault();
+        output = "";
         const categoryId = this.id;
+        console.log("data found: making ajax call !", categoryId);
+
+        // making ajax call
         $.ajax({
             type: "get",
             url: "/",
@@ -10,10 +14,12 @@ $(document).ready(() => {
             },
             dataType: "json",
             success: function(response) {
-                console.log("response is : ", response);
-                $(".product__display").replaceWith(
-                    "<h1>Here is the required data !" + response + " </h1>"
-                );
+                const allUseRecords = response;
+                for (record in allUseRecords) {
+                    output += "<h1>" + response.model + "</h1>";
+                }
+
+                $(".product__display").html(output);
             },
         });
     });
