c9a2d65dfb0869204a946d58e3de1494553a6d5b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-19 17:16:57 +0530

    response sent back

--- store/static/store/js/product.js
+++ store/static/store/js/product.js
@@ -1,6 +1,20 @@
 $(document).ready(() => {
     $(".list__filter").click(function(e) {
-        console.log(this.id);
         e.preventDefault();
+        const categoryId = this.id;
+        $.ajax({
+            type: "get",
+            url: "/",
+            data: {
+                category_id: categoryId,
+            },
+            dataType: "json",
+            success: function(response) {
+                console.log("response is : ", response);
+                $(".product__display").replaceWith(
+                    "<h1>Here is the required data !" + response + " </h1>"
+                );
+            },
+        });
     });
 });
--- store/views.py
+++ store/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Product, Category
+from django.http import JsonResponse
+from django.core import serializers
 
 
 def home_view(request):
-    product_objs = Product.get_all_products()
-    category_objs = Category.get_all_categories()
-    context = {
-        'products': product_objs,
-        'categories': category_objs,
-    }
-    return render(request, 'store/home.html', context)
+    if request.method == "GET" and request.is_ajax():
+        json_obj = request.GET.get("category_id", None)
+        if json_obj is not None:
+            product_objs = get_list_or_404(Product, category=json_obj)
+            qs_json = serializers.serialize('json', product_objs)
+            return JsonResponse(data=qs_json, safe=False)
+    else:
+        product_objs = Product.get_all_products()
+        category_objs = Category.get_all_categories()
+        context = {
+            'products': product_objs,
+            'categories': category_objs,
+        }
+        return render(request, 'store/home.html', context)
