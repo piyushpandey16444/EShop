7dd6421ffb26c676cad315920fc8e208fb1bfac7
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-19 16:23:01 +0530

    js added

---
+++ store/static/store/js/product.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $("#category__filter").click(() => {
+        const clickedFilter = $("#category__filter");
+        console.log("coming", clickedFilter.val());
+    });
+});
