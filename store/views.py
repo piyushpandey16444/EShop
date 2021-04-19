bbd6201e242ef1e0b30fd8f0cb102a9cdb3c9556
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-19 20:55:23 +0530

    serializers created

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Product, Category
 from django.http import JsonResponse
 from django.core import serializers
+from .serializers import ProductSerializer, CategorySerializer
 
 
 def home_view(request):
@@ -9,8 +10,10 @@ def home_view(request):
         json_obj = request.GET.get("category_id", None)
         if json_obj is not None:
             product_objs = get_list_or_404(Product, category=json_obj)
-            qs_json = serializers.serialize('json', product_objs)
-            return JsonResponse(data=qs_json, safe=False)
+            print("product_objs: ", product_objs)
+            serializer = ProductSerializer(product_objs)
+            print("serializer : ", serializer.data)
+            return JsonResponse(data=serializer.data, safe=False)
     else:
         product_objs = Product.get_all_products()
         category_objs = Category.get_all_categories()
