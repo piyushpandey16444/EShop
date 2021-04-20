1c697adcf966056739b77924588e706bb4af0af4
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-21 00:59:55 +0530

    filter done, working on url

--- store/views.py
+++ store/views.py
@@ -7,45 +7,32 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-def compute_order(obtained_category=None, price_order=None):
-    if not obtained_category:
-        # send all product order by
-        if price_order == 'l2h':
-            product_objs = Product.get_product_by_category(
-                category_name=None).order_by('price')
-        else:
-            product_objs = Product.get_product_by_category(
-                category_name=None).order_by('-price')
+def compute_order(get_category=None, get_price=None):
+    if get_price == 'l2h':
+        product_objs = Product.get_product_by_category(
+            category_name=get_category).order_by('price')
     else:
-        # send category wise dt order by
-        if price_order == 'l2h':
-            product_objs = Product.get_product_by_category(
-                category_name=obtained_category).order_by('price')
-        else:
-            product_objs = Product.get_product_by_category(
-                category_name=obtained_category).order_by('-price')
+        product_objs = Product.get_product_by_category(
+            category_name=get_category).order_by('-price')
     return product_objs
 
 
 def home_view(request):
-    if request.method == "GET" and request.is_ajax():
+    if request.method == "GET":
         category_objs = Category.get_all_categories()
-        obtained_category = request.GET.get('category', None)
-        price_order = request.GET.get('price_order', None)
-        product_objs = compute_order(
-            obtained_category=obtained_category, price_order=price_order)
-        context = {
-            'products': product_objs,
-            'categories': category_objs,
-        }
-        return render(request, 'store/home.html', context)
 
-    elif request.method == "GET":
-        print(request.GET)
-        # print(r)
-        category_objs = Category.get_all_categories()
         get_category = request.GET.get('category_name', None)
         get_price = request.GET.get('price', None)
+
+        if get_price and get_category:
+            product_objs = compute_order(
+                get_category=get_category, get_price=get_price)
+            context = {
+                'products': product_objs,
+                'categories': category_objs,
+            }
+            return render(request, 'store/home.html', context)
+
         if get_category is not None:
             product_objs = Product.get_product_by_category(
                 category_name=get_category)
