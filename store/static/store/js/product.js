c5121ca2d91ef56103f3d18e72ace59c008c1050
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-22 00:46:44 +0530

    ajax implimented for all products and l2h

--- store/static/store/js/product.js
+++ store/static/store/js/product.js
@@ -14,22 +14,28 @@ $(document).ready(() => {
             dataType: "json",
             success: function(response) {
                 const allUseRecords = response;
-                console.log("response is: ", allUseRecords);
-                for (record in allUseRecords) {
+                console.log("response is: ", allUseRecords["response"]);
+                for (record in allUseRecords["response"]) {
                     output +=
-                        "<div class='row mx-auto'>" +
-                        "<div class='card shadow p-3 mb-5 bg-white rounded mx-auto mb-3' style='width: 18rem;'>" +
+                        "<div class='d-inline-flex'><div class='no-gutters'>" +
+                        "<div class='card shadow p-3 mb-5 bg-white rounded ml-3' style='width: 18rem;'>" +
                         "<img class='card-img-top' style='width: 100%; height: 20rem;' " +
                         "src = 'http://localhost:8000/images/" +
-                        allUseRecords[record]["image"] +
+                        allUseRecords["response"][record]["image"] +
                         "'/> " +
                         "<div class='card-body'>" +
-                        "<h5 class='card-title'></h5>" +
-                        "<p class='card-text' style='width: 100%; height: 4rem;'><br></p></div>" +
+                        "<h5 class='card-title'>" +
+                        allUseRecords["response"][record]["name"] +
+                        "</h5>" +
+                        "<p class='card-text' style='width: 100%; height: 4rem;'>" +
+                        allUseRecords["response"][record]["price"] +
+                        "<br>" +
+                        allUseRecords["response"][record]["description"] +
+                        "</p></div>" +
                         "<a href='#' class='btn btn-outline-secondary btn-lg'>" +
                         "Add To Cart" +
                         "</a>" +
-                        "</div></div>";
+                        "</div></div></div>";
                 }
 
                 $(".product__body").html(output);
