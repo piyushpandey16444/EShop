214df69ec8b242dfc08b15c13937d78fe691536f
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-21 23:40:21 +0530

    implimented ajax views

--- store/views.py
+++ store/views.py
@@ -7,8 +7,8 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 
 
-def compute_order(get_category=None, get_price=None):
-    if get_price == 'l2h':
+def compute_order(get_category=None, get_filter=None):
+    if get_filter == 'l2h':
         product_objs = Product.get_product_by_category(
             category_name=get_category).order_by('price')
     else:
@@ -19,22 +19,15 @@ def compute_order(get_category=None, get_price=None):
 
 def home_view(request):
     if request.method == "GET" and request.is_ajax():
-        pass
+        product_objs = compute_order(
+            get_category=None, get_filter=request.GET.get('get_filter'))
+        return JsonResponse(data={'response': list(product_objs.values())}, safe=False)
+
     elif request.method == "GET":
         category_objs = Category.get_all_categories()
 
         get_category = request.GET.get('category_name', None)
         get_price = request.GET.get('price', None)
-
-        if get_price and get_category:
-            product_objs = compute_order(
-                get_category=get_category, get_price=get_price)
-            context = {
-                'products': product_objs,
-                'categories': category_objs,
-            }
-            return render(request, 'store/home.html', context)
-
         if get_category is not None:
             product_objs = Product.get_product_by_category(
                 category_name=get_category)
