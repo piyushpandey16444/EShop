bda3337c9f0bfdccd6628fbfeda3aeda4b2cea8b
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-22 01:11:51 +0530

    ajax category wise h2l

--- store/static/store/js/product.js
+++ store/static/store/js/product.js
@@ -58,6 +58,30 @@ $(document).ready(() => {
                 dataType: "json",
                 success: function(response) {
                     console.log("response for category wise product l2h is: ", response);
+                    const allUseRecords = response;
+                    for (record in allUseRecords["response"]) {
+                        output +=
+                            "<div class='d-inline-flex'><div class='no-gutters'>" +
+                            "<div class='card shadow p-3 mb-5 bg-white rounded ml-3' style='width: 18rem;'>" +
+                            "<img class='card-img-top' style='width: 100%; height: 20rem;' " +
+                            "src = 'http://localhost:8000/images/" +
+                            allUseRecords["response"][record]["image"] +
+                            "'/> " +
+                            "<div class='card-body'>" +
+                            "<h5 class='card-title'>" +
+                            allUseRecords["response"][record]["name"] +
+                            "</h5>" +
+                            "<p class='card-text' style='width: 100%; height: 4rem;'>" +
+                            allUseRecords["response"][record]["price"] +
+                            "<br>" +
+                            allUseRecords["response"][record]["description"] +
+                            "</p></div>" +
+                            "<a href='#' class='btn btn-outline-secondary btn-lg'>" +
+                            "Add To Cart" +
+                            "</a>" +
+                            "</div></div></div>";
+                    }
+                    $(".product__body").html(output);
                 },
             });
         }
