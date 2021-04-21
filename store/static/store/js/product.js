1168a3423412df595f8e29c64f359abfd00923e0
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-22 00:08:14 +0530

    getting image by ajax

--- store/static/store/js/product.js
+++ store/static/store/js/product.js
@@ -17,6 +17,19 @@ $(document).ready(() => {
                 console.log("response is: ", allUseRecords);
                 for (record in allUseRecords) {
                     output +=
+                        "<div class='row mx-auto'>" +
+                        "<div class='card shadow p-3 mb-5 bg-white rounded mx-auto mb-3' style='width: 18rem;'>" +
+                        "<img class='card-img-top' style='width: 100%; height: 20rem;' " +
+                        "src = 'http://localhost:8000/images/" +
+                        allUseRecords[record]["image"] +
+                        "'/> " +
+                        "<div class='card-body'>" +
+                        "<h5 class='card-title'></h5>" +
+                        "<p class='card-text' style='width: 100%; height: 4rem;'><br></p></div>" +
+                        "<a href='#' class='btn btn-outline-secondary btn-lg'>" +
+                        "Add To Cart" +
+                        "</a>" +
+                        "</div></div>";
                 }
 
                 $(".product__body").html(output);
