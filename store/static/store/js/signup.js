55c437a4dd7347c74ec9ad28d599ae21f8312a45
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-23 01:00:05 +0530

    username field styling completed

--- store/static/store/js/signup.js
+++ store/static/store/js/signup.js
@@ -1,7 +1,7 @@
-$(document).ready(() => {
-    $(".ap_customer_uname").click(function(e) {
-        e.preventDefault();
-        $(".ap_customer_uname").css("box-shadow", "0px 0px 5px 2px #f09f26");
-        $(".ap_customer_uname").css("outline-color", "#c97800");
-    });
-});
+// $(document).ready(() => {
+//     $(".ap_customer_uname").click(function(e) {
+//         e.preventDefault();
+//         $(".ap_customer_uname").css("box-shadow", "0px 0px 5px 2px #f09f26");
+//         $(".ap_customer_uname").css("outline-color", "#c97800");
+//     });
+// });
