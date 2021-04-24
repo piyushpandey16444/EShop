436e391bdcaf0be19b18fcc5542e5cd29d6b73b8
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-24 14:21:49 +0530

    forms added to signup page

--- store/forms.py
+++ store/forms.py
@@ -1,11 +1,37 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class UserAdminCreationForm(UserCreationForm):
     """
     A Custom form for creating new users.
     """
+    email = forms.CharField(max_length=76, required=True, widget=forms.EmailInput(attrs={
+        'class': 'ap_email',
+        'tabindex': 1,
+        'autocomplete':
+        'email', 'id':
+        'ap_email'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "ap_password1",
+        "tabindex": 2,
+        "autocomplete": "off",
+        "maxlength": 1024,
+        "id": "ap_password1",
+        "placeholder": "At least 8 characters"
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "ap_password2",
+        "tabindex": 3,
+        "autocomplete": "off",
+        "maxlength": 1024,
+        "id": "ap_password2",
+        "placeholder": "Should be same as password"
+    }))
 
     class Meta:
         model = get_user_model()
